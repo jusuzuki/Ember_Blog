eb10990d324820ee5dbca2576ba5b8f521a51e51
Momo Ozawa & Juliana Suzuki <student@epicodus.com>
2015-09-30 11:42:28 -0700

    Delete working properly

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    delete(post) {
+        if (confirm('Are you sure you want to delete this post?')) {
+          this.sendAction('destroyPost', post);
+        }
+      }
+    }
+
 });
