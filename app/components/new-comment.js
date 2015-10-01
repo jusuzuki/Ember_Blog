fe003569cd869e42a7367bd52417ac001b2da577
Juliana <student@epicodus.com>
2015-10-01 15:40:59 -0700

    Add date to comments

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewComment: false,
+  addNewComment: true,
   actions: {
     commentFormShow() {
       this.set('addNewComment', true);
@@ -12,6 +12,7 @@ export default Ember.Component.extend({
         user: this.get('user') ? this.get('user') : "",
         text: this.get('text') ? this.get('text') : "",
         post: this.get('post') ? this.get('post') : "",
+        date_added: Date.now()
       };
       this.set('addNewComment', false);
       this.sendAction('saveComment', params);
--- app/models/comment.js
+++ app/models/comment.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     user: DS.attr(),
     text: DS.attr(),
+    date_added: DS.attr(),
     post: DS.belongsTo('post', { async: true })
 });
